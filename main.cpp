471abee1956532fd27207d9bf461a3338e2878c5
happylife15-vasu <71512666+happylife15-vasu@users.noreply.github.com>
2020-09-28 20:14:39 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int num=0, per=0;
+printf("Enter your percentage: ");
+scanf("%d",&per);
+
+if(per>80)
+printf("A grade");
+else if(per<80 && per>=70)
+printf("B grade");
+else if(per<70 && per>=60)
+printf("C grade");
+else if(per<60 && per>=45)
+printf("D grade");
+
+else
+printf("FAIL!");
+
+
+return 0;
+
+}
